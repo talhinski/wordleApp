49332aa928b2b90900837d6674ade13325d35df7
talhinski <talhinski@gmail.com>
2025-02-08 19:34:20 +0200

    read words first steps for wordel layout

--- app/src/main/java/com/hinski/wordelapplication/MainActivity.java
+++ app/src/main/java/com/hinski/wordelapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.hinski.wordelapplication;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,14 +8,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.util.Log;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        //read the words list from the file
+        try {
+            Resources res = getResources();
+            InputStream in_s = res.openRawResource(R.raw.words);
+            BufferedReader reader = new BufferedReader(new java.io.InputStreamReader(in_s));
+            String line;
+            List<String> wordsList = new ArrayList<String>();
+            while ((line = reader.readLine()) != null) {
+                //split the words by the comma
+                String[] words = line.split(",");
+                if (words.length > 1) {
+                    wordsList.add(words[1]);
+                }
+
+            }
+            WordelWords wordelWords = new WordelWords(wordsList);
+            Log.i(TAG, "onCreate: " + wordelWords.words);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
---
+++ app/src/main/java/com/hinski/wordelapplication/WordelWords.java
@@ -0,0 +1,12 @@
+package com.hinski.wordelapplication;
+
+import java.util.List;
+
+public class WordelWords {
+    //gets in the constructor a list of words
+    List<String> words;
+
+    public WordelWords(List<String> words) {
+        this.words = words;
+    }
+}
